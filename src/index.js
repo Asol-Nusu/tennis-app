890c60475ec635004684fe0709fddae779115c90
Asol Nusuvalieva <nusuvalieva_a@auca.kg>
2024-01-02 02:23:16 +0600

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById("root"));
